102e8199cbae732f74b4082b5c2b54f1ccd2f0e2
zhongwx <zhongwx@mama.cn>
2023-04-20 20:49:11 +0800

    [modify]

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -173,7 +173,6 @@ import { HttpPostData } from '@/api'
 import { UploadFile } from '@/api'
 import Recorder from 'js-audio-recorder'
 
-// const lamejs = require('lamejs')
 const recorder = new Recorder({
           sampleBits: 16,               
           sampleRate: 16000,             
@@ -319,39 +318,6 @@ export default {
       that.mp3_url = res.data.split("/")[3]
       console.log(res,that.mp3_url)
     },
-
-    // convertToMp3(wavDataView) {
-    //     // 获取wav头信息
-    //     const wav = lamejs.WavHeader.readHeader(wavDataView); // 此处其实可以不用去读wav头信息，毕竟有对应的config配置
-    //     const { channels, sampleRate } = wav;
-    //     const mp3enc = new lamejs.Mp3Encoder(channels, sampleRate, 128);
-    //     // 获取左右通道数据
-    //     const result = recorder.getChannelData()
-    //     const buffer = [];
-    //     const leftData = result.left && new Int16Array(result.left.buffer, 0, result.left.byteLength / 2);
-    //     const rightData = result.right && new Int16Array(result.right.buffer, 0, result.right.byteLength / 2);
-    //     const remaining = leftData.length + (rightData ? rightData.length : 0);
-    //     const maxSamples = 1152;
-    //     for (let i = 0; i < remaining; i += maxSamples) {
-    //       const left = leftData.subarray(i, i + maxSamples);
-    //       let right = null;
-    //       let mp3buf = null;
-    //       if (channels === 2) {
-    //         right = rightData.subarray(i, i + maxSamples);
-    //         mp3buf = mp3enc.encodeBuffer(left, right);
-    //       } else {
-    //         mp3buf = mp3enc.encodeBuffer(left);
-    //       }
-    //       if (mp3buf.length > 0) {
-    //         buffer.push(mp3buf);
-    //       }
-    //     }
-    //     const enc = mp3enc.flush();
-    //     if (enc.length > 0) {
-    //       buffer.push(enc);
-    //     }
-    //     return new Blob(buffer, { type: 'audio/mp3' });
-    //  },
 
     async getLesson () {
       let that = this
