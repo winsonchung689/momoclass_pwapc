596df13ce6f295d38cde58aeab2608a4ed040143
zhongwx <zhongwx@mama.cn>
2023-04-30 09:58:20 +0800

    [modify]

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -26,6 +26,31 @@
 </template>
 
 <script>
+
+window.onload = function(){
+    document.addEventListener('touchstart',function(event){
+        if(event.touches.length>1){
+            event.preventDefault()
+        }
+    });
+
+    document.addEventListener('gesturestart',function(event){
+        if(event.touches.length>1){
+            event.preventDefault()
+        }
+    });
+
+    var lastTouchEnd = 0;
+    document.documentElement.addEventListener('touchend', function(event){
+        var now =  (new Date()).getTime()
+        if(now - lastTouchEnd <= 300){
+            event.preventDefault()
+        }
+        lastTouchEnd = now
+    },false)
+}
+
+
 import { HttpPost } from '@/api'
 
 export default {
