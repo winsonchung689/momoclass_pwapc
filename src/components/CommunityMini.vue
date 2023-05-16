29c359f27a8c0b37026c0c506b002e1edb003743
zhongwx <zhongwx@mama.cn>
2023-05-16 16:16:46 +0800

    [modify]

--- src/components/CommunityMini.vue
+++ src/components/CommunityMini.vue
@@ -59,10 +59,10 @@
 
 
                 <div style="margin-left: 50%;display: flex;flex-direction: row;">
-                  <el-badge value="new" class="comment_item">
+                  <el-badge :value="item.like_amount" class="comment_item">
                     <el-button @click="warning()" size="small">点赞</el-button>
                   </el-badge>
-                  <el-badge value="new" class="comment_item">
+                  <el-badge :value="item.comment_amount" class="comment_item">
                     <el-button @click="warning()" size="small">评论</el-button>
                   </el-badge>
                 </div>
