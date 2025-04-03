164dbf1e5b3580523d7e640502a19d4fe6ac0ceb
Winson <oncwnuEyoufyCrWoOUrox1LOZN9s@git.weixin.qq.com>
2025-04-03 09:03:06 +0800

    [modify]

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -304,7 +304,7 @@ methods: {
         }
 
         that.nick_name = users.data[0].nick_name
-        that.avatarurl = ImageUrl + users.data[0].avatarurl.split("/")[5]
+        that.avatarurl = ImageUrl + users.data[0].avatarurl
         that.role = users.data[0].role
         that.subject = users.data[0].subjects
         that.comment_style = users.data[0].comment_style
